644ab6c52de05e6bf0be64348c06f9111b3a1ae9
darth0s <zwyklydachowiec@gmail.com>
2017-06-30 14:20:21 +0000

    Add files via upload
    auto launcher for the scripts

---
+++ launcher.bat
@@ -0,0 +1,14 @@
+@echo off
+echo *****  Starting bitlucky ******
+echo ################################
+casperjs bitlucky_io.js
+
+echo ***** Starting bituniverse ******
+echo ################################
+casperjs bituniverse_io.js
+
+echo *****  Starting Plansads  ******
+echo ################################
+casperjs plansads_com.js
+
+pause
